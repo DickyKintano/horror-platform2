ed0477516f51ae4363fc692a4bc7b924cc3511f1
George Matthew <george02@terpmail.umd.edu>
2020-03-18 10:35:37 -0400

    Add player-door interaction

---
+++ HorrorGame/Assets/DoorInteraction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+
+public class DoorInteraction : MonoBehaviour
+{
+    [SerializeField] private GameObject connectedRoom;
+    [SerializeField] private GameObject currRoom;
+    [SerializeField] private CinemachineVirtualCamera mainCamera;
+    [SerializeField] private GameObject player;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            connectedRoom.SetActive(true);
+            currRoom.SetActive(false);
+            mainCamera.Follow = player.transform;
+            mainCamera.LookAt = player.transform;
+        }
+    }
+
+}
---
+++ HorrorGame/Assets/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D playerRB;
+    [SerializeField] private float moveSpeed = 40f;
+
+    private float movementSmoothing = 0.05f;
+    private float xMove;
+    private float yMove;
+    private Vector2 velocity = Vector2.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (playerRB == null)
+        {
+            playerRB = GetComponent<Rigidbody2D>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xMove = Input.GetAxisRaw("Horizontal");
+        yMove = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        Move(xMove * moveSpeed * Time.deltaTime, yMove * moveSpeed * Time.deltaTime);
+
+        
+    }
+
+    private void Move(float x, float y)
+    {
+        Vector2 movement = new Vector2(x * 10f, y * 10f);
+
+        playerRB.velocity = Vector2.SmoothDamp(playerRB.velocity, movement, ref velocity, movementSmoothing);
+
+        //animation down here
+    }
+
+
+}
